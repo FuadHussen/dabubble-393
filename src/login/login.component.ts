98fd93cdf3cf872311be94b471b412fbca9f6b81
Sascha Lenz <lenz.sascha.1994@gmail.com>
2025-01-22 16:58:02 +0100

    add login component

--- src/login/login.component.ts
+++ src/login/login.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
 
 @Component({
   selector: 'app-login',
-  imports: [MatSlideToggleModule],
+  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
+  animations: [
+    trigger('moveAnimation', [
+      state('center', style({ transform: 'translate(0, 0)' })),
+      state('moveToCorner', style({ transform: 'translate(-40vw, -40vh) scale(0.5)' })),
+    ]),
+    trigger('logoFadeAnimation', [
+      state('center', style({ opacity: 0,scale:0.5 })),//start
+      state('moveToCorner', style({ opacity: 1,scale:1.5 })),//ende
+      transition('center => moveToCorner', animate('2s ease')),//zeit dazwischen
+    ]),
+    trigger('nameFadeAnimation', [
+      state('center', style({ opacity: 0 })),
+      state('moveToCorner', style({ opacity: 1, transform: 'translate(24px, 0)' })),
+      transition('center => moveToCorner', animate('1s ease-in-out')), // Delay of 1s
+    ]),
+  ],
 })
 export class LoginComponent {
+  animationState = 'center';
+  backgroundColor = 'blue';
 
+  ngOnInit() {
+    setTimeout(() => {
+      this.animationState = 'moveToCorner';
+    }, 60);
+  }
 }
