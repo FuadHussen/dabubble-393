de4e5e83ff37a7f1edfa586b36ab96c771907db8
Sascha Lenz <lenz.sascha.1994@gmail.com>
2025-01-27 15:10:37 +0100

    add signup and resetpassword comp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { Sidenav2Component } from './sidenav-2/sidenav-2.component';
+import { ResetPasswordComponent } from './login/reset-password/reset-password.component';
+import { SignupComponent } from './login/signup/signup.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
+    { path: 'reset-password', component: ResetPasswordComponent },
+    { path: 'signup', component: SignupComponent },
     { path: 'sidenav', component: SidenavComponent },
     { path: 'sidenav-2', component: Sidenav2Component },
 ];
