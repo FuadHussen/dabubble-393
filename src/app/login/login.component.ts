318fc0d20e7d250773abe28f6c293ce5cb5171d2
Sascha Lenz <lenz.sascha.1994@gmail.com>
2025-01-29 11:55:36 +0100

    password forgot add and footer

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,29 +1,44 @@
 import { Component } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
+import { Router } from '@angular/router';
+import { FooterComponent } from '../shared/footer/footer.component';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
   animations: [
     trigger('moveAnimation', [
       state('center', style({ transform: 'translate(0, 0)' })),
-      state('moveToCorner', style({ transform: 'translate(-40vw, -40vh) scale(0.5)' })),
+      state(
+        'moveToCorner',
+        style({ transform: 'translate(-40vw, -40vh) scale(0.5)' })
+      ),
     ]),
     trigger('logoFadeAnimation', [
-      state('center', style({ opacity: 0,scale:0.5 })),//start
-      state('moveToCorner', style({ opacity: 1,scale:1.5 })),//ende
-      transition('center => moveToCorner', animate('2s ease')),//zeit dazwischen
+      state('center', style({ opacity: 0, scale: 0.5 })), //start
+      state('moveToCorner', style({ opacity: 1, scale: 1.5 })), //ende
+      transition('center => moveToCorner', animate('2s ease')), //zeit dazwischen
     ]),
     trigger('nameFadeAnimation', [
       state('center', style({ opacity: 0 })),
-      state('moveToCorner', style({ opacity: 1, transform: 'translate(24px, 0)' })),
+      state(
+        'moveToCorner',
+        style({ opacity: 1, transform: 'translate(24px, 0)' })
+      ),
       transition('center => moveToCorner', animate('1s ease-in-out')), // Delay of 1s
     ]),
   ],
 })
 export class LoginComponent {
+  constructor(private router: Router) {}
   animationState = 'center';
   backgroundColor = 'blue';
 
@@ -51,5 +66,13 @@ export class LoginComponent {
 
   onPasswordBlur() {
     this.isPasswordFocused = false;
+  }
+
+  navigateToSignUp() {
+    this.router.navigate(['/signup']); // Hier kannst du den gewünschten Pfad angeben
+  }
+
+  navigateToForgotPassword() {
+    this.router.navigate(['/reset-password']); // Hier kannst du den gewünschten Pfad angeben
   }
 }
--- src/app/login/reset-password/reset-password.component.ts
+++ src/app/login/reset-password/reset-password.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../../shared/footer/footer.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-reset-password',
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './reset-password.component.html',
-  styleUrl: './reset-password.component.scss'
+  styleUrl: './reset-password.component.scss',
 })
 export class ResetPasswordComponent {
+    constructor(private router: Router) {}
+  isEmailFocused = false;
+  imgSrc: string = '../../../assets/img/arrow-back.png';
 
+  // Methoden zum Setzen des Fokusstatus
+  onEmailFocus() {
+    this.isEmailFocused = true;
+  }
+
+  onEmailBlur() {
+    this.isEmailFocused = false;
+  }
+
+  changeImage(state: string) {
+    if (state === 'hover') {
+      this.imgSrc = '../../../assets/img/arrow-back-active.png';
+    } else {
+      this.imgSrc = '../../../assets/img/arrow-back.png';
+    }
+  }
+
+  navigateToLogin() {
+    this.router.navigate(['/login']); // Hier kannst du den gewünschten Pfad angeben
+  }
 }
