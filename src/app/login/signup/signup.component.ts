e57c2ebdce9d2dbd5be8b724a8074682a60a6a38
Sascha Lenz <lenz.sascha.1994@gmail.com>
2025-01-29 14:19:51 +0100

    konto erstellen side1

--- src/app/login/signup/signup.component.ts
+++ src/app/login/signup/signup.component.ts
@@ -1,11 +1,77 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.scss'
+  styleUrl: './signup.component.scss',
 })
 export class SignupComponent {
+  constructor(private router: Router) {}
 
+  userName: string = '';
+  userEmail: string = '';
+  userPassword: string = '';
+
+  arrowBackSrc: string = '../../../assets/img/arrow-back.png';
+
+  userNameSrc: string = '../../assets/img/person.png';
+  userEmailSrc: string = '../../assets/img/mail.png';
+  userPasswordSrc: string = '../../assets/img/lock.png';
+
+  userNameError: boolean = false;
+
+  onFocus(field: string): void {
+    if (field === 'userName' && !this.userName) {
+      this.userNameSrc = '../../assets/img/person-active.png';
+    } else if (field === 'userEmail' && !this.userEmail) {
+      this.userEmailSrc = '../../assets/img/mail-active.png';
+    } else if (field === 'userPassword' && !this.userPassword) {
+      this.userPasswordSrc = '../../assets/img/lock-active.png';
+    }
+  }
+
+  onBlur(field: string): void {
+    if (field === 'userName' && !this.userName) {
+      this.userNameSrc = '../../assets/img/person.png';
+    } else if (field === 'userEmail' && !this.userEmail) {
+      this.userEmailSrc = '../../assets/img/mail.png';
+    } else if (field === 'userPassword' && !this.userPassword) {
+      this.userPasswordSrc = '../../assets/img/lock.png';
+    }
+  }
+
+  onInput(field: string, event: Event): void {
+    let value = (event.target as HTMLInputElement).value;
+    if (field === 'userName') {
+      this.userName = value;
+      this.userNameSrc = value
+        ? '../../assets/img/person-active.png'
+        : '../../assets/img/person.png';
+    } else if (field === 'userEmail') {
+      this.userEmail = value;
+      this.userEmailSrc = value
+        ? '../../assets/img/mail-active.png'
+        : '../../assets/img/mail.png';
+    } else if (field === 'userPassword') {
+      this.userPassword = value;
+      this.userPasswordSrc = value
+        ? '../../assets/img/lock-active.png'
+        : '../../assets/img/lock.png';
+    }
+  }
+
+  navigateToLogin() {
+    this.router.navigate(['/login']);
+  }
+
+  changeImage(state: string) {
+    if (state === 'hover') {
+      this.arrowBackSrc = '../../../assets/img/arrow-back-active.png';
+    } else {
+      this.arrowBackSrc = '../../../assets/img/arrow-back.png';
+    }
+  }
 }
