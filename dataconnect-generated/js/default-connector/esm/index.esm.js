f85df8d7a8742867215ea742ef07223f1098b587
FuadHussen <142115071+FuadHussen@users.noreply.github.com>
2025-01-31 13:39:15 +0100

    added firebase

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { , validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'dabubble-393',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,9 @@
+const { , validateArgs } = require('firebase/data-connect');
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'dabubble-393',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
+
